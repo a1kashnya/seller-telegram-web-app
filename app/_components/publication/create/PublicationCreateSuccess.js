49c03d903569c9e5f387cb2d43d761f2cfedd6bd
a1kashnya <k.popkoff@yandex.ru>
2024-09-16 01:54:28 +0400

    Add publication/create success message

--- app/_components/publication/create/PublicationCreateSuccess.js
+++ app/_components/publication/create/PublicationCreateSuccess.js
@@ -4,9 +4,11 @@ const CreateSuccess = () => {
     return (
         <div>
             <div className="grid gap-3 border-t border-b py-5 border-slate-800">
-                <h2 className="text-white text-center text-2xl">Ваша акция <b>имя акции</b> создана успешно!</h2>
-                <div className="text-white text-center mb-3">Теперь нужно дождаться модерации. Все публикации мы тщательно проверяем, но обычно это занимает не больше часа. Ожидайте.</div>
-                <Link className="block w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-4" href="/">Вернуться</Link>
+                <span className="border-l-[6px] border-blue-500 bg-[#3b82f630] p-3 pl-4 rounded-lg text-white mb-2">
+                    <h2 className="text-white text-center text-2xl mb-2">Ваша акция <b>имя акции</b> создана успешно!</h2>
+                    <div className="text-white text-center mb-5">Теперь нужно дождаться модерации. Все публикации мы тщательно проверяем, но обычно это занимает не больше часа. Ожидайте.</div>
+                    <Link className="block w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-4" href="/">Вернуться</Link>
+                </span>
             </div>
         </div>
     )
