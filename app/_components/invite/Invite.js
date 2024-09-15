6ae97918f03d5e7923526e10d1882132395a7cb4
a1kashnya <k.popkoff@yandex.ru>
2024-09-16 01:58:28 +0400

    Add invite message

--- app/_components/invite/Invite.js
+++ app/_components/invite/Invite.js
@@ -14,11 +14,13 @@ const Invite = () => {
         <>
             <Title name="Пригласи друга" btn />
             <div className="grid gap-3 border-t border-b py-5 border-slate-800 text-slate-200">
-                <span>Чтобы открыть доступ к приглашениям - у вас должна быть хотя бы 1 успешно завершенная публикация</span>
-                <span>Максимум приглашений в день 5</span>
-                <span>1 сгенерированный код = одному приглашению. По нему сможет подключиться только ОДИН пользователь</span>
+                <div className="flex flex-col border-l-[6px] border-blue-500 bg-[#3b82f630] p-3 pl-4 rounded-lg text-white mb-2">
+                    <span className='mb-2 font-medium'>Чтобы открыть доступ к приглашениям - у вас должна быть хотя бы 1 успешно завершенная публикация.</span>
+                    <span className='mb-2'>Максимум приглашений в день (5).</span>
+                    <span className='mb-5'>1 сгенерированный код = одному приглашению. По нему сможет подключиться только ОДИН пользователь</span>
+                    <button onClick={toggleModal} className="block w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-4">Сгенерировать код</button>
+                </div>
             </div>
-            <button onClick={toggleModal} className="block w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-4">Сгенерировать код</button>
             <InviteModal showModal={isModalOpen} onClose={toggleModal} />
         </>
     )
