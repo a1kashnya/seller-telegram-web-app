7a842fa1cae19061b14246bad2507309864c79c5
a1kashnya <k.popkoff@yandex.ru>
2024-09-16 01:59:48 +0400

    Add help message

--- app/_components/help/Help.js
+++ app/_components/help/Help.js
@@ -3,13 +3,15 @@ import Title from "@/_components/_ui/Title"
 const Help = () => {
   return (
     <div>
-            <Title name="Нужда помощь?" btn/>
-            <div className="grid gap-3 border-t border-b py-5 border-slate-800">
-                <span className="text-slate-300">Если у вас есть вопросы - можете обратиться в нашу тех. поддержку.</span>
-                <span className="text-slate-300">Контакты: @support_lavka_dobbi</span>
-                <span className="text-slate-300">График работы: Понедельник - Пятница 9:00 - 21:00</span>
-            </div>
+      <Title name="Нужда помощь?" btn />
+      <div className="grid gap-3 border-t border-b py-5 border-slate-800">
+        <div className="flex flex-col border-l-[6px] border-blue-500 bg-[#3b82f630] p-3 pl-4 rounded-lg text-white mb-2">
+          <span className="text-slate-300 mb-2 font-medium">Если у вас есть вопросы - можете обратиться в нашу тех. поддержку.</span>
+          <span className="text-slate-300"><b>Контакты:</b> @support_lavka_dobbi</span>
+          <span className="text-slate-300"><b>График работы:</b> Понедельник - Пятница 9:00 - 21:00</span>
         </div>
+      </div>
+    </div>
   )
 }
 
