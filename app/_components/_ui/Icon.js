5d413750fce97a1a5fc30046d794e87664a14830
a1kashnya <k.popkoff@yandex.ru>
2024-09-18 13:01:57 +0400

    Fix icon size

--- app/_components/_ui/Icon.js
+++ app/_components/_ui/Icon.js
@@ -1,13 +1,13 @@
 import * as Icons from '@heroicons/react/24/solid';
 
-const Icon = ({ name, size }) => {
+const Icon = ({ name, size = 6 }) => {
     const Component = Icons[name]
 
     if (!Component) {
         return
     }
 
-    return <Component className={`size-${size}`} />
+    return <Component className={`size-6`} />
 }
 
 export default Icon
