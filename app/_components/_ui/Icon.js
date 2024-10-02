eb54f7962b4248ebaa2885f362952eb2e94ec9f3
a1kashnya <k.popkoff@yandex.ru>
2024-10-02 17:04:31 +0400

    Improved the icon code

--- app/_components/_ui/Icon.js
+++ app/_components/_ui/Icon.js
@@ -1,13 +1,13 @@
 import * as Icons from '@heroicons/react/24/solid';
 
-const Icon = ({ name, size = 6 }) => {
+const Icon = ({ name, size, color }) => {
     const Component = Icons[name]
 
     if (!Component) {
         return
     }
 
-    return <Component className={`size-6`} />
+    return <Component className={`${size ? `h-${size} w-${size}` : 'size-6'} ${color}`} />
 }
 
 export default Icon
