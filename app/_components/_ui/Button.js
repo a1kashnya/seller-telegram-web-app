f8af249bfe6dd2a585c11be8b400304068015c73
a1kashnya <k.popkoff@yandex.ru>
2024-10-02 17:03:44 +0400

    Improved the button code

--- app/_components/_ui/Button.js
+++ app/_components/_ui/Button.js
@@ -2,17 +2,17 @@ import Link from "next/link"
 import Icon from "@/_components/_ui/Icon"
 
 const Button = ({ type, name, icon, url, bgColor, textColor, handle = null }) => {
+    const baseClasses = `${bgColor} ${textColor} flex justify-center items-center gap-2 w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-3`
+
     return (
         <>
-            {type === 'link' && (
-                <Link className={`${bgColor} ${textColor} flex justify-center items-center gap-2 w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-3`} href={url}>
-                    <Icon name={icon} size={6}/>{name}
+            {type === 'link' ? (
+                <Link className={baseClasses} href={url}>
+                    <Icon name={icon} />{name}
                 </Link>
-            )}
-
-            {type === 'button' && (
-                <button className="flex justify-center items-center gap-1 w-full bg-blue-500 text-blue-100 text-xs text-center rounded-2xl font-medium uppercase px-2 py-3" onClick={handle}>
-                    <Icon name={icon} size={6}/>{name}
+            ) : (
+                <button className={baseClasses} onClick={handle}>
+                    <Icon name={icon} />{name}
                 </button>
             )}
         </>
